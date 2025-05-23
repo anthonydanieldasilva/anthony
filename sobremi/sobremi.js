c5550e75b2c1c1d2759eef3983137adcb04e7b69
anthony <anthonydasilva2012@gmail.com>
2025-05-22 22:07:07 -0300

    newbgjavaanimation

--- sobremi/sobremi.js
+++
@@ -1,8 +0,0 @@
-var bioShowMore = document.getElementById('show-more-icon');
-var bioMoreWrap = document.getElementById('profile-bio-wrapper');
-
-if (typeof(bioShowMore) != 'undefined' && bioShowMore != null){
-	bioShowMore.addEventListener('click', function(){
-		bioMoreWrap.classList.toggle('show-more-info');
-	});
-}
